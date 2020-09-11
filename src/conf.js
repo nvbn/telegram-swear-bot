5cf1c8be1c9351a1cad57043f0ac336e50ec4c0e
Afanaciev Kirill <afanasiev.kirill@gmail.com>
2020-09-11 09:31:18 +0300

    Бот стал менее дерзким

--- src/conf.js
+++ src/conf.js
@@ -1,1 +1,3 @@
-export const TELEGRAM_TOKEN: string = process.env.TELEGRAM_TOKEN;
+//export const TELEGRAM_TOKEN: string = process.env.TELEGRAM_TOKEN;
+export const TELEGRAM_TOKEN: string = '1307686008:AAFtVj-FxT96VT5tyYOHHVgdI1mDzGZo0DU';
+
